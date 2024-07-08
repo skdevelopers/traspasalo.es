c5c6588feb8394c985870c620381f6e6cff0a162
mudaser <uafmcs@gmail.com>
2024-07-08 19:06:19 +0500

    update remove cdn link 08-07-2024

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -404,7 +404,6 @@
 @endsection
 
 @push('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 
     {{-- Top banner carousel Start --}}
     <script>
