4252d755a192a22f89ea4c569a4b294ee20456a5
mudaser <uafmcs@gmail.com>
2024-08-08 19:06:14 +0500

    updated User Model

---
+++ database/migrations/2024_08_08_140415_set_default_value_for_model_type_in_model_has_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('model_has_roles', function (Blueprint $table) {
+            // Set default value for model_type column
+            $table->string('model_type')->default('App\Models\User')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('model_has_roles', function (Blueprint $table) {
+            // Remove default value for model_type column
+            $table->string('model_type')->default(null)->change();
+        });
+    }
+};
