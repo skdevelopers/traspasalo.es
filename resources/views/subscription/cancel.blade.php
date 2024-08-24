1da329ed9e7c9034971b5cfe843a42f98a1bf52d
mudaser <uafmcs@gmail.com>
2024-08-24 20:19:12 +0500

    update

--- resources/views/subscription/cancel.blade.php
+++ resources/views/subscription/cancel.blade.php
@@ -1,5 +1,10 @@
-<h1>Payment Cancelled</h1>
+@extends('layouts.vertical', ['title' => 'Payment Message', 'sub_title' => 'Payment'])
+
+@section('content')
+<h1 class="text-red-500 text-center text-2xl">Payment Cancelled</h1>
 <p>Your payment was not completed. You can try again or contact support.</p>
+    
+@endsection
 
 
 
--- resources/views/subscription/success.blade.php
+++ resources/views/subscription/success.blade.php
@@ -1,2 +1,6 @@
-<h1>Payment Successful</h1>
+@extends('layouts.vertical', ['title' => 'Payment Message', 'sub_title' => 'Payment'])
+
+@section('content')
+<h1 class="text-green-500 text-center text-2xl">Payment Successful</h1>
 <p>Thank you for your purchase! Your subscription is now active.</p>
+@endsection
