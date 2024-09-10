90de486a3eae06baa3f9559a5985e23e2aed8726
mudaser <uafmcs@gmail.com>
2024-09-10 16:58:04 +0500

    updated frontend

--- resources/views/front/partials/nav.blade.php
+++ resources/views/front/partials/nav.blade.php
@@ -75,7 +75,7 @@
                     <div
                         class="fc-dropdown fc-dropdown-open:opacity-100 hidden opacity-0 w-44 z-50 transition-[margin,opacity] duration-300 mt-2 bg-white shadow-lg border rounded-lg p-2 border-gray-200 dark:border-gray-700 dark:bg-gray-800">
                         <a class="flex items-center py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300"
-                            href="pages-gallery.html">
+                            href="#">
                             <span>
                                 @if (Auth::check())
                                     <p>Hi, {{ Auth::user()->first_name }}</p>
@@ -85,8 +85,17 @@
 
                             </span>
                         </a>
-
-                        <form method="POST" action="{{ route('lock-screen') }}">
+                        
+                            
+                            <a href="{{ route('home') }}"
+                            class="flex items-center py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
+                            <button type="submit">
+                                <i class="mgc_lock_line  me-2"></i>
+                                <span>DashBoard</span>
+                            </button>
+                        </a>
+                        
+                        {{-- <form method="POST" action="{{ route('lock-screen') }}">
                             @csrf
                             <a
                                 class="flex items-center py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
@@ -95,7 +104,8 @@
                                     <span>Lock Screen</span>
                                 </button>
                             </a>
-                        </form>
+                        </form> --}}
+
                         <hr class="my-2 -mx-2 border-gray-200 dark:border-gray-700">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
